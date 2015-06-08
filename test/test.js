6a85378774d0b4ecc9ee5f23b7c705be14190fd8
koopero <koopero@gmail.com>
2015-06-08 12:47:10 -0700

    0.0.1

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ const
   scratch = resolve.bind( this, __dirname, 'scratch' ),
   rimraf = require('rimraf'),
   sync = mod.sync,
+  touch = mod.touch,
   touchSync = mod.touchSync,
   format = mod.format,
   existsSync = fs.existsSync
@@ -21,6 +22,40 @@ describe('#format', function() {
     eq( format('%Y%M%D - %h%m%s', '', 0, date ), '610412 - 060700' );
   });
 
+  it('will format seconds from Date', function () {
+    var date = new Date(2.06673340006673*1000); // 2 seconds + 2 NTSC frames
+    eq( format('%s', '', 0, date ), '02' );
+    eq( format('%.s', '', 0, date ), '2.066' );
+  });
+
+  it('will format seconds from Date', function () {
+    var date = new Date(2.06673340006673*1000); // 2 seconds + 2 NTSC frames
+    eq( format('%s', '', 0, date ), '02' );
+    eq( format('%.s', '', 0, date ), '2.066' );
+  });
+
+  it('will format seconds from Date', function () {
+    var date = new Date(2.06673340006673*1000); // 2 seconds + 2 NTSC frames
+    eq( format('%s', '', 0, date ), '02' );
+    eq( format('%.s', '', 0, date ), '2.066' );
+  });
+
+  it('will format Date as time', function () {
+    var time = new Date('14 April 1981, 18:20:57 UTC');
+    eq( format('%t', '', 0, time ), '356120457000' );
+    eq( format('%06t', '', 0, time ), '457000' );
+    eq( format('%3t', '', 0, time ), '356120457000' );
+    eq( format('%04.2t', '', 0, time ), '7000.00' );
+  });
+
+  it('will format Number as time', function () {
+    var num = 12345.6789;
+    eq( format('%01.5t', '', 0, num ), '5.67890' );
+    eq( format('%6.2t', '', 0, num ), '012345.68' );
+
+  });
+
+
   it('will format ascii filenames', function () {
     var fileWithDir = 'dirname/basename.extension';
     var file = 'basename.extension';
@@ -32,7 +67,7 @@ describe('#format', function() {
     eq( format('%4e', file ), '.ext' );
   })
 
-  it('will format decimal numbers', function () {
+  it('will format decimal indexes', function () {
     eq( format('%i', '', 1000 ), '1000' );
     eq( format('%4i', '', 3 ), '0003' );
     eq( format('%4i', '', 11111 ), '11111' );
@@ -41,25 +76,26 @@ describe('#format', function() {
     eq( format('%5i', '', 0 ), '00000' );
   });
 
-  it('will format other radixes', function () {
+  it('will format decimal indexes in other radixes', function () {
     eq( format('%z', '', 255 ), 'ff' );
-    eq( format('%3t', '', 1000 ), '0rs' );
-  });
-
-  it('will format decimal numbers', function () {
-    eq( format('%i', '', 1000 ), '1000' );
-    eq( format('%4i', '', 3 ), '0003' );
-    eq( format('%4i', '', 11111 ), '11111' );
-    eq( format('%i', '', 0 ), '' );
-    eq( format('%0i', '', 0 ), '0' );
-    eq( format('%5i', '', 0 ), '00000' );
   });
 
 
-  it('will slugify filenames', function () {
-    eq( format('%b', '.file' ), 'file' );
-    eq( format('%b', 'i hate spaces in file names' ), 'i_hate_spaces_in_file_names' );
-    eq( format('%b', '.htaccess' ), 'htaccess' );
+  it('will remove special characters from filenames', function () {
+    // Period is only removed at start of the line.
+    eq( format('%f', '.file.1.ext' ), 'file.1.ext' );
+    eq( format('%f', 'i hate spaces in file names' ), 'i_hate_spaces_in_file_names' );
+  })
+
+  it('will remove non-ascii characters from filenames', function () {
+    // Period is only removed at start of the line.
+    ass( !hasNonASCII( format('%f', 'Frenchy McFucknuts.ext' ) ) );
+    ass( !hasNonASCII( format('%f', 'ᓱᓴᓐ ᐊᒡᓗᒃᑲᖅ.ext' ) ) );
+    console.log( format('%f', '.ᓱᓴᓐ ᐊᒡᓗᒃᑲᖅ.ext' ) );
+  })
+
+  it('will remove potentially dangerous characters', function () {
+    eq( format('%b', "it's a file!" ), 'its_a_file' );
   })
 
   it('will format randoms', function () {
@@ -71,13 +107,29 @@ describe('#format', function() {
   });
 });
 
+describe('touch', function () {
+  it('should touch a file', function ( cb ) {
+    var file = scratch('touch/async');
+    touch( file, function ( err ) {
+      ass( !err );
+      ass( existsSync( file ) );
+      cb();
+    } );
+  })
+});
+
 describe('touchSync', function () {
   it('should touch a file', function () {
     var file = scratch('foo/bar');
-    touchSync( file );
+    ass( touchSync( file ), 'did not return true');
 
     var stat = fs.statSync( file );
     eq( stat.size, 0 );
+  });
+
+  it("should return false when it can't", function () {
+    var file = scratch('foo/bar/foo');
+    ass( !touchSync( file ) );
   });
 
   after( wipeScratch );
@@ -118,4 +170,15 @@ describe('uniqueFileName', function ( cb ) {
 
 function wipeScratch() {
   rimraf.sync( scratch() );
+}
+
+function hasNonASCII ( str ) {
+  var buf = new Buffer( str, 'utf8' );
+  var result = false;
+  for ( var i = 0; i < buf.length; i ++ ) {
+    //console.log( i, str[i],buf.readUInt8(i))
+    result = result || buf.readUInt8(i) >= 128;
+  }
+
+  return result;
 }
